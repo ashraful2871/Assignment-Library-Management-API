92062f6e29a64a991095df8c033d27cac78862a6
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-02 22:19:10 +0600

    add cors origin

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const book_controller_1 = require("./app/controller/book.controller");
 const borrow_controller_1 = require("./app/controller/borrow.controller");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/api/books", book_controller_1.booksRouts);
 app.use("/api/borrow", borrow_controller_1.borrowRouts);
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from "express";
 import { booksRouts } from "./app/controller/book.controller";
 import { borrowRouts } from "./app/controller/borrow.controller";
-
+import cors from "cors";
 const app: Application = express();
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/api/books", booksRouts);
 app.use("/api/borrow", borrowRouts);
 
